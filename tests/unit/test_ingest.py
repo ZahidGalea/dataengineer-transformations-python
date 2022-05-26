2739211034b86e3ff266d8c757ee490ca0c6b633
vn53nag <zahid.galea@walmart.com>
2022-05-26 01:50:30 -0300

    Fixes ingest column sanitization tests

--- tests/unit/test_ingest.py
+++ tests/unit/test_ingest.py
@@ -2,27 +2,18 @@ from data_transformations.citibike import ingest
 
 
 def test_should_sanitize_nothing() -> None:
-    no_whitespace_columns = ['foo']
-
-    actual = ingest.sanitize_columns(no_whitespace_columns)
-    expected = no_whitespace_columns
-
-    assert expected == actual
+    input = ['foo']
+    expected = ['foo']
+    assert expected == ingest.sanitize_columns(input)
 
 
 def test_should_sanitize_whitespace_outside() -> None:
-    no_whitespace_columns = [' foo ']
-
-    actual = ingest.sanitize_columns(no_whitespace_columns)
+    input = [' foo ']
     expected = ['_foo_']
-
-    assert expected == actual
+    assert expected == ingest.sanitize_columns(input)
 
 
 def test_should_sanitize_whitespace_in_between() -> None:
-    no_whitespace_columns = ['foo bar']
-
-    actual = ingest.sanitize_columns(no_whitespace_columns)
+    input = ['foo bar']
     expected = ['foo_bar']
-
-    assert expected == actual
+    assert expected == ingest.sanitize_columns(input)
